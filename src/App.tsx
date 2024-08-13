821b2454728a0d8c0873f31c617e2bb8b6670511
Dhyan Rai <dhyanraibm@gmail.com>
2024-08-13 15:39:04 +0530

    Resume updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import { backendExperience, frontendExperience, miniProjects, projects } from './constants';
-import './index.css';
+import {
+  backendExperience,
+  frontendExperience,
+  miniProjects,
+  projects,
+} from './constants'
+import './index.css'
 
 const App = () => {
-
   return (
     <div>
       <nav id="desktop-nav">
         <div className="logo">Dhyan Rai</div>
         <div>
           <ul className="nav-links">
-            <li><a href="#about">About</a></li>
-            <li><a href="#experience">Experience</a></li>
-            <li><a href="#projects">Projects</a></li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#experience">Experience</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
           </ul>
         </div>
       </nav>
-      
-      <section id="profile" >
-        <div className="section__pic-container" id="proImg" >
-          <img src="/assets/p-pic.svg" alt="John Doe profile picture" style={{borderRadius:"100%",borderTop:"1px solid black"}} />
+
+      <section id="profile">
+        <div className="section__pic-container" id="proImg">
+          <img
+            src="/assets/p-pic-real1.jpeg"
+            alt="Dhyan Rai profile picture Here"
+            style={{
+              maxHeight: '320px',
+              maxWidth: '320px',
+              borderRadius: '100%',
+              objectFit: 'cover',
+            }}
+          />
         </div>
         <div className="section__text">
           <p className="section__text__p1">Hello, I'm</p>
@@ -31,7 +50,12 @@ const App = () => {
             >
               Download CV
             </button>
-            <button className="btn btn-color-1" onClick={() => window.location.href = 'mailto:dhyanraibm@gmail.com'}>
+            <button
+              className="btn btn-color-1"
+              onClick={() =>
+                (window.location.href = 'mailto:dhyanraibm@gmail.com')
+              }
+            >
               Contact
             </button>
           </div>
@@ -40,13 +64,18 @@ const App = () => {
               src="/assets/linkedin.png"
               alt="My LinkedIn profile"
               className="icon"
-              onClick={() => window.location.href = 'https://www.linkedin.com/in/dhyan-rai-bm/'}
+              onClick={() =>
+                (window.location.href =
+                  'https://www.linkedin.com/in/dhyan-rai-bm/')
+              }
             />
             <img
               src="/assets/github.png"
               alt="My Github profile"
               className="icon"
-              onClick={() => window.location.href = 'https://github.com/DhyanRaiBM'}
+              onClick={() =>
+                (window.location.href = 'https://github.com/DhyanRaiBM')
+              }
             />
           </div>
         </div>
@@ -64,7 +93,10 @@ const App = () => {
                   className="icon"
                 />
                 <h3>Experience</h3>
-                <p>1 year of <br />FullStack Development</p>
+                <p>
+                  1 year of <br />
+                  FullStack Development
+                </p>
               </div>
               <div className="details-container">
                 <img
@@ -73,13 +105,20 @@ const App = () => {
                   className="icon"
                 />
                 <h3>Education</h3>
-                <p>Bachelor's in Computer Applications<br /></p>
+                <p>
+                  Bachelor's in Computer Applications
+                  <br />
+                </p>
               </div>
             </div>
             <div className="text-container">
               <p>
-                
-                Hello, I'm a passionate developer who enjoys creating things and solving problems. Whether it's a new project or just everyday stuff, I'm always curious and love learning new things. Whether it's building user-friendly interfaces or optimizing backend functionality, I'm ready for new challenges and opportunities to make an impact.
+                Hello, I'm a passionate developer who enjoys creating things and
+                solving problems. Whether it's a new project or just everyday
+                stuff, I'm always curious and love learning new things. Whether
+                it's building user-friendly interfaces or optimizing backend
+                functionality, I'm ready for new challenges and opportunities to
+                make an impact.
               </p>
             </div>
           </div>
@@ -95,7 +134,7 @@ const App = () => {
           src="/assets/arrow.png"
           alt="Arrow icon"
           className="icon arrow"
-          onClick={() => window.location.href = './#experience'}
+          onClick={() => (window.location.href = './#experience')}
         />
       </section>
       <section id="experience">
@@ -106,8 +145,8 @@ const App = () => {
             <div className="details-container">
               <h2 className="experience-sub-title">Frontend Development</h2>
               <div className="article-container">
-                {frontendExperience.map((exp,idx)=>(
-                  <article key={idx} >
+                {frontendExperience.map((exp, idx) => (
+                  <article key={idx}>
                     <img
                       src={exp.imgSrc}
                       alt="Experience icon"
@@ -124,19 +163,19 @@ const App = () => {
             <div className="details-container">
               <h2 className="experience-sub-title">Backend and Tools </h2>
               <div className="article-container">
-                {backendExperience.map((exp,idx)=>(
-                    <article key={idx} >
-                      <img
-                        src={exp.imgSrc}
-                        alt="Experience icon"
-                        className="icon"
-                      />
-                      <div>
-                        <h3>{exp.title}</h3>
-                        <p>{exp.level}</p>
-                      </div>
-                    </article>
-                  ))}
+                {backendExperience.map((exp, idx) => (
+                  <article key={idx}>
+                    <img
+                      src={exp.imgSrc}
+                      alt="Experience icon"
+                      className="icon"
+                    />
+                    <div>
+                      <h3>{exp.title}</h3>
+                      <p>{exp.level}</p>
+                    </div>
+                  </article>
+                ))}
               </div>
             </div>
           </div>
@@ -145,7 +184,7 @@ const App = () => {
           src="/assets/arrow.png"
           alt="Arrow icon"
           className="icon arrow"
-          onClick={() => window.location.href = './#projects'}
+          onClick={() => (window.location.href = './#projects')}
         />
       </section>
       <section id="projects">
@@ -153,96 +192,109 @@ const App = () => {
         <h1 className="title">Projects</h1>
         <div className="experience-details-container">
           <div className="project-container">
-            {projects.map((project)=>(
-            <div className="details-container color-container">
-              <div className="article-container">
-                <img
-                  src={project.imgSrc}
-                  alt={project.imgAlt}
-                  className="project-img"
-                />
-              </div>
-              <h2 className="experience-sub-title project-title">{project.projectTitle}</h2>
-              <div className="btn-container">
-                <button
-                  className="btn btn-color-2"
-                  onClick={() => window.open(project.sourceCodeLink)}
-                >
-                  Source Code
-                </button>
-                {project.liveDemoLink && 
+            {projects.map((project) => (
+              <div className="details-container color-container">
+                <div className="article-container">
+                  <img
+                    src={project.imgSrc}
+                    alt={project.imgAlt}
+                    className="project-img"
+                  />
+                </div>
+                <h2 className="experience-sub-title project-title">
+                  {project.projectTitle}
+                </h2>
+                <div className="btn-container">
                   <button
-                    className="btn btn-color-1"
-                    onClick={() => window.open(project.liveDemoLink)}
+                    className="btn btn-color-2"
+                    onClick={() => window.open(project.sourceCodeLink)}
                   >
-                    Live Demo
+                    Source Code
                   </button>
-                }
+                  {project.liveDemoLink && (
+                    <button
+                      className="btn btn-color-1"
+                      onClick={() => window.open(project.liveDemoLink)}
+                    >
+                      Live Demo
+                    </button>
+                  )}
+                </div>
               </div>
-            </div>
             ))}
-            
           </div>
         </div>
         <img
           src="/assets/arrow.png"
           alt="Arrow icon"
           className="icon arrow"
-          onClick={() => window.location.href = './#contact'}
+          onClick={() => (window.location.href = './#contact')}
         />
       </section>
       <section id="projects">
         <p className="section__text__p1">Projects built by me as a learner :</p>
-        <p className="section__text__p1">Projects may take upto a minute to load, Please wait.</p>
+        <p className="section__text__p1">
+          Projects may take upto a minute to load, Please wait.
+        </p>
         {/* <h1 className="title">Projects</h1> */}
         <div className="experience-details-container">
           <div className="sub-project-container">
-            {miniProjects.map((project)=>(
-            <div className="details-container color-container">
-              <div className="article-container">
-                <img
-                  src={project.imgSrc}
-                  alt={project.imgAlt}
-                  className="project-img"
-                />
-              </div>
-              <h2 style={{fontSize:"20px"}} className="experience-sub-title project-title">{project.projectTitle}</h2>
-              <div className="btn-container">
-                <button
-                  className="btn1 btn-color-2"
-                  onClick={() => window.open(project.sourceCodeLink)}
+            {miniProjects.map((project) => (
+              <div className="details-container color-container">
+                <div className="article-container">
+                  <img
+                    src={project.imgSrc}
+                    alt={project.imgAlt}
+                    className="project-img"
+                  />
+                </div>
+                <h2
+                  style={{ fontSize: '20px' }}
+                  className="experience-sub-title project-title"
                 >
-                  Source Code
-                </button>
-                {project.liveDemoLink && 
+                  {project.projectTitle}
+                </h2>
+                <div className="btn-container">
                   <button
-                    className="btn btn-color-1"
-                    onClick={() => window.open(project.liveDemoLink)}
+                    className="btn1 btn-color-2"
+                    onClick={() => window.open(project.sourceCodeLink)}
                   >
-                    Live Demo
+                    Source Code
                   </button>
-                }
+                  {project.liveDemoLink && (
+                    <button
+                      className="btn btn-color-1"
+                      onClick={() => window.open(project.liveDemoLink)}
+                    >
+                      Live Demo
+                    </button>
+                  )}
+                </div>
               </div>
-            </div>
             ))}
-            
           </div>
         </div>
       </section>
-      <footer className='footer'>
-      <nav>
-        <div className="nav-links-container">
-          <ul className="nav-links">
-            <li><a href="#about">About</a></li>
-            <li><a href="#experience">Experience</a></li>
-            <li><a href="#projects">Projects</a></li>
-          </ul>
-        </div>
-      </nav>
-      <p>Copyright &#169; 2024 Dhyan Rai. All Rights Reserved.</p>
-    </footer>
+      <footer className="footer">
+        <nav>
+          <div className="nav-links-container">
+            <ul className="nav-links">
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#experience">Experience</a>
+              </li>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <p>Copyright &#169; 2024 Dhyan Rai. All Rights Reserved.</p>
+      </footer>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
