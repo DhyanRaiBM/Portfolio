20f07f256b4208110cb42af8e6e177c1bd75bc01
Dhyan Rai <dhyanraibm@gmail.com>
2024-09-09 13:11:56 +0530

    Project update

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,191 +1,187 @@
 interface Experience {
-  imgSrc: string;
-  altText: string;
-  title: string;
-  level: string;
+  imgSrc: string
+  altText: string
+  title: string
+  level: string
 }
 
-export const frontendExperience:Experience[] = [
-    {
-        imgSrc: "./assets/checkmark.png",
-        altText: "Experience icon",
-        title: "HTML5",
-        level: "Experienced"
-    },
-    {
-        imgSrc: "./assets/checkmark.png",
-        altText: "Experience icon",
-        title: "CSS",
-        level: "Experienced"
-    },
-    {
-        imgSrc: "./assets/checkmark.png",
-        altText: "Experience icon",
-        title: "Tailwind",
-        level: "Intermediate"
-    },
-    {
-        imgSrc: "./assets/checkmark.png",
-        altText: "Experience icon",
-        title: "Javascript",
-        level: "Experienced"
-    },
-    {
-        imgSrc: "./assets/checkmark.png",
-        altText: "Experience icon",
-        title: "TypeScript",
-        level: "Basic"
-    },
-    {
-        imgSrc: "./assets/checkmark.png",
-        altText: "Experience icon",
-        title: "ReactJS",
-        level: "Intermediate"
-    },
-    {
-        imgSrc: "./assets/checkmark.png",
-        altText: "Experience icon",
-        title: "Material UI",
-        level: "Intermediate"
-    },
-    {
-        imgSrc: "./assets/checkmark.png",
-        altText: "Experience icon",
-        title: "ShadCN UI",
-        level: "Intermediate"
-    }
-];
+export const frontendExperience: Experience[] = [
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'HTML5',
+    level: 'Experienced',
+  },
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'CSS',
+    level: 'Experienced',
+  },
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'Tailwind',
+    level: 'Intermediate',
+  },
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'Javascript',
+    level: 'Experienced',
+  },
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'TypeScript',
+    level: 'Basic',
+  },
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'ReactJS',
+    level: 'Intermediate',
+  },
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'Material UI',
+    level: 'Intermediate',
+  },
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'ShadCN UI',
+    level: 'Intermediate',
+  },
+]
 
-export const backendExperience:Experience[] = [
-    {
-      imgSrc: "./assets/checkmark.png",
-      altText: "Experience icon",
-      title: "PostgreSQL",
-      level: "Basic"
-    },
-    {
-      imgSrc: "./assets/checkmark.png",
-      altText: "Experience icon",
-      title: "Node JS",
-      level: "Intermediate"
-    },
-    {
-      imgSrc: "./assets/checkmark.png",
-      altText: "Experience icon",
-      title: "Express JS",
-      level: "Intermediate"
-    },
-    {
-      imgSrc: "./assets/checkmark.png",
-      altText: "Experience icon",
-      title: "MongoDB",
-      level: "Intermediate"
-    },
-    {
-      imgSrc: "./assets/checkmark.png",
-      altText: "Experience icon",
-      title: "NextJS",
-      level: "Intermediate"
-    },
-    {
-      imgSrc: "./assets/checkmark.png",
-      altText: "Experience icon",
-      title: "Postman",
-      level: "Intermediate"
-    },
-    {
-      imgSrc: "./assets/checkmark.png",
-      altText: "Experience icon",
-      title: "Stripe",
-      level: "Intermediate"
-    },
-    {
-      imgSrc: "./assets/checkmark.png",
-      altText: "Experience icon",
-      title: "Git",
-      level: "Intermediate"
-    }
-];
+export const backendExperience: Experience[] = [
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'PostgreSQL',
+    level: 'Basic',
+  },
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'Node JS',
+    level: 'Intermediate',
+  },
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'Express JS',
+    level: 'Intermediate',
+  },
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'MongoDB',
+    level: 'Intermediate',
+  },
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'NextJS',
+    level: 'Intermediate',
+  },
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'Postman',
+    level: 'Intermediate',
+  },
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'Stripe',
+    level: 'Intermediate',
+  },
+  {
+    imgSrc: './assets/checkmark.png',
+    altText: 'Experience icon',
+    title: 'Git',
+    level: 'Intermediate',
+  },
+]
 
 export const projects = [
   {
-    imgSrc: "/assets/project-1.png",
-    imgAlt: "AI Image Enhancer",
-    projectTitle: "AI Image Enhancer",
-    sourceCodeLink: "https://github.com/DhyanRaiBM/Next-cloudinary.git",
-    liveDemoLink: "https://next-cloudinary-git-main-dhyan-rais-projects.vercel.app/",
+    imgSrc: '/assets/project-1.png',
+    imgAlt: 'AI Image Enhancer',
+    projectTitle: 'AI Image Enhancer',
+    sourceCodeLink: 'https://github.com/DhyanRaiBM/Next-cloudinary.git',
+    liveDemoLink:
+      'https://next-cloudinary-git-main-dhyan-rais-projects.vercel.app/',
   },
   {
-    imgSrc: "/assets/project-4.png",
-    imgAlt: "ViewTube",
-    projectTitle: "ViewTube",
-    sourceCodeLink: "https://github.com/DhyanRaiBM/ReactJS-ViewTube.git",
-    liveDemoLink: "https://viewtube-dhyan.netlify.app/",
+    imgSrc: '/assets/project-4.png',
+    imgAlt: 'ViewTube',
+    projectTitle: 'ViewTube',
+    sourceCodeLink: 'https://github.com/DhyanRaiBM/ReactJS-ViewTube.git',
+    liveDemoLink: 'https://viewtube-dhyan.netlify.app/',
   },
   {
-    imgSrc: "/assets/project-3.png",
-    imgAlt: "MediaHub API",
-    projectTitle: "MediaHub API",
-    sourceCodeLink: "https://github.com/DhyanRaiBM/NodeJS-Media-Hub-API.git",
+    imgSrc: '/assets/project-3.png',
+    imgAlt: 'MediaHub API',
+    projectTitle: 'MediaHub API',
+    sourceCodeLink: 'https://github.com/DhyanRaiBM/NodeJS-Media-Hub-API.git',
     // liveDemoLink: "https://github.com/",
   },
   {
-    imgSrc: "/assets/project-2.png",
-    imgAlt: "Housing Portal",
-    projectTitle: "Housing Portal",
-    sourceCodeLink: "https://github.com/DhyanRaiBM/ReactJS-State.git",
-    liveDemoLink: "https://react-js-state.vercel.app/",
+    imgSrc: '/assets/project-2.png',
+    imgAlt: 'Housing Portal',
+    projectTitle: 'Housing Portal',
+    sourceCodeLink: 'https://github.com/DhyanRaiBM/ReactJS-State.git',
+    liveDemoLink: 'https://react-js-state.vercel.app/',
   },
   {
-    imgSrc: "/assets/project-5.png",
-    imgAlt: "Stripe Store",
-    projectTitle: "Stripe Store",
-    sourceCodeLink: "https://github.com/DhyanRaiBM/StripeStore-Nextjs.git",
-    liveDemoLink: "https://stripe-store-dhyan.vercel.app/",
+    imgSrc: '/assets/project-5.png',
+    imgAlt: 'Stripe Store',
+    projectTitle: 'Stripe Store',
+    sourceCodeLink: 'https://github.com/DhyanRaiBM/StripeStore-Nextjs.git',
+    liveDemoLink: 'https://stripe-store-dhyan.vercel.app/',
   },
   {
-    imgSrc: "/assets/project-6.png",
-    imgAlt: "Upquarium Finance",
-    projectTitle: "Upquarium Finance",
-    sourceCodeLink: "https://github.com/DhyanRaiBM/EJS---UpquariumFinance.git",
-    liveDemoLink: "https://upquarium-finance.onrender.com/",
+    imgSrc: '/assets/project-6.png',
+    imgAlt: 'Upquarium Finance',
+    projectTitle: 'Upquarium Finance',
+    sourceCodeLink: 'https://github.com/DhyanRaiBM/EJS---UpquariumFinance.git',
+    liveDemoLink: 'https://ejs-upquariumfinance-production.up.railway.app/',
   },
-  
-  // Add more project objects here
-];
-
+]
 
 export const miniProjects = [
   {
-    imgSrc: "/assets/mini-project-1.png",
-    imgAlt: "Simple Todo",
-    projectTitle: "Simple Todo",
-    sourceCodeLink: "https://github.com/DhyanRaiBM/EJS---UpquariumFinance.git/",
-    liveDemoLink: "https://todolist-zyy1.onrender.com/",
+    imgSrc: '/assets/mini-project-1.png',
+    imgAlt: 'Simple Todo',
+    projectTitle: 'Simple Todo',
+    sourceCodeLink: 'https://github.com/DhyanRaiBM/EJS---UpquariumFinance.git/',
+    liveDemoLink: 'https://todolist-zyy1.onrender.com/',
   },
   {
-    imgSrc: "/assets/mini-project-2.png",
-    imgAlt: "Weather App",
-    projectTitle: "Weather App",
-    sourceCodeLink: "https://github.com/DhyanRaiBM/checkWeather.git",
-    liveDemoLink: "https://checkweather-production.up.railway.app/",
+    imgSrc: '/assets/mini-project-2.png',
+    imgAlt: 'Weather App',
+    projectTitle: 'Weather App',
+    sourceCodeLink: 'https://github.com/DhyanRaiBM/checkWeather.git',
+    liveDemoLink: 'https://checkweather-production.up.railway.app/',
   },
   {
-    imgSrc: "/assets/mini-project-3.png",
-    imgAlt: "Blog Platform",
-    projectTitle: "Blog Platform",
-    sourceCodeLink: "https://github.com/DhyanRaiBM/BlogEjs.git",
-    liveDemoLink: "https://blog-app-a8bv.onrender.com/",
+    imgSrc: '/assets/mini-project-3.png',
+    imgAlt: 'Blog Platform',
+    projectTitle: 'Blog Platform',
+    sourceCodeLink: 'https://github.com/DhyanRaiBM/BlogEjs.git',
+    liveDemoLink: 'https://blog-app-a8bv.onrender.com/',
   },
   {
-    imgSrc: "/assets/mini-project-4.png",
-    imgAlt: "Cartoonize API",
-    projectTitle: "Cartoonize API",
-    sourceCodeLink: "https://github.com/DhyanRaiBM/cartoonize-api.git",
+    imgSrc: '/assets/mini-project-4.png',
+    imgAlt: 'Cartoonize API',
+    projectTitle: 'Cartoonize API',
+    sourceCodeLink: 'https://github.com/DhyanRaiBM/cartoonize-api.git',
     // liveDemoLink: "https://github.com/",
   },
-  
+
   // Add more project objects here
-];
-
-
+]
